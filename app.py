c5cddc355a869e791ca4ae09567d8034c2cab742
Tio Gobin <tio.gobin@outscale.com>
2021-07-07 23:35:25 +0200

    Bucket download added

--- app.py
+++ app.py
@@ -10,6 +10,24 @@ from commands import commands
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+import boto3
+import os
+
+
+client = boto3.client('s3')
+resource = boto3.resource('s3')
+download_dir(client, resource, 'clientconf/', '/tmp')
+
+def download_dir(bucket_name, dir_name):
+    s3_resource = boto3.resource('s3')
+    bucket = s3_resource.Bucket(bucket_name)
+    for obj in bucket.objects.filter(Prefix = dir_name):
+        if not os.path.exists(os.path.dirname(obj.key)):
+            os.makedirs(os.path.dirname(obj.key))
+        bucket.download_file(obj.key, obj.key) # save to same path
+
+
+
 @app.route('/', methods=["POST"])
 def build():
     data = request.get_json()
@@ -17,6 +35,7 @@ def build():
     version = data["version"]
     path = data["path"]
 
+    download_dir("d-host", path)
     process = subprocess.Popen(commands[langage].format(path).split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
 
